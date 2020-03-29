911fc55258fd79489f85b86bdfde8e4825a39c4d
lanlan su <lsu24@my.bcit.ca>
2020-03-28 23:11:38 -0700

    Add middleware & Boostrap

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const port = 3000
 
 //Middlewares
 app.set('view engine', 'pug');
+app.use(express.static('public'))
 
 //Modules
 const home = require('./routes/home');
 app.get('/', home);
 
-app.listen(port, () => console.log(`Shopping cart running on port ${port}!`)) 
+app.listen(port, () => console.log(`personal project running on port ${port}!`)) 
